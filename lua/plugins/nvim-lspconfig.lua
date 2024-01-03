47f6ab0e8402b2ab93c87560eae3de7ad3324d3c
James Kelly <james.kelly.99@gmail.com>
2024-01-03 11:49:28 +1100

    add rosyln analyzers for better actions

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -13,4 +13,9 @@ return {
 		-- Additional lua configuration, makes nvim stuff amazing!
 		'folke/neodev.nvim',
 	},
+	config = function()
+		require 'lspconfig'.omnisharp.setup {
+			enable_roslyn_analyzers = true
+		}
+	end
 }
